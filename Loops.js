6f2ac57e464e79ec432e1bff1f80e8aa4aa73f84
Simeon Sena <ssena@assurecare.com>
2021-09-08 18:56:50 -0400

    upload module 3

--- Loops.js
+++ Loops.js
@@ -1,3 +1,30 @@
 for (let i = 0; i<=10;i++){
-console.log(i);
+      console.log('outer loop: ',i);
+       for(let j = 10; j >=0; j-=2) {
+       console.log('  inner loop: ', j);
+            }
+      
+         }
+
+         const gameBoard = [
+            [ 4, 32, 8, 4 ],
+            [ 64, 8, 32, 2 ],
+            [ 8, 32, 16, 4 ],
+            [ 2, 8, 4, 2 ]
+      ];
+      
+      let totalScore = 0;
+      for(let i = 0; i < gameBoard.length; i++) {
+          let row = gameBoard[i];
+          
+          for(let j = 0; j < row.length; j++) {
+          totalScore += row[j];      
+          }
       }
+      const target1 = Math.floor(Math.random() * 10);
+      let guess1 = Math.floor(Math.random() * 10);
+      while(guess1 !== target1){
+          console.log(`${target1} ${guess1}`)
+          guess1 = Math.floor(Math.random() * 10);
+      }
+      console.log(`${target1} ${guess1}`)     
---
+++ module3.js
@@ -0,0 +1,6 @@
+let colors =['red', 'blue', 'purple']
+//for (let i =0; i <colors.length; i++)
+
+for(let color of colors){
+    console.log(color);
+}
