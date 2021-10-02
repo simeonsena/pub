cf75ba4f99ef02627af2832b8e637c1059ca624e
Simeon Sena <ssena@assurecare.com>
2021-10-02 01:54:26 -0400

    get file reader working

--- main.js
+++ main.js
@@ -1,12 +1,141 @@
+// const topTextInput = document.querySelector('#topTextInput')
+// const bottomTextInput = document.querySelector('#bottomTextInput')
+// const canvas = document.querySelector('#meme')
+// const input = document.querySelector("#imageFileInput");
+// //const input = document.querySelector('input[type="file"]')
+// // const img = new Image();
+// // input.addEventListener('change', function(e){
 
-const input = document.querySelector('input[type="file"]')
-input.addEventListener('change', function(e){
-  //  console.log(input.files)
-const reader = new FileReader()
-reader.onload = function(){
-    const img = new Image()
-    img.src = reader.result
-    document.body.appendChild(img)
+// // const reader = new FileReader()
+// // reader.onload = function(){
+    
+// //     img.src = reader.result;
+// //     document.body.appendChild(img);
+// //     updateMemeCanvas(topTextInput.value, bottomTextInput.value,canvas, img);
+// // }
+// //     reader.readAsDataURL(input.files[0])
+// // }, false)
+
+
+// let img;
+
+// input.addEventListener("change", (e) => {
+//   const imageDataUrl = URL.createObjectURL(e.target.files[0]);
+
+//   img = new Image();
+//   img.src = imageDataUrl;
+
+//   img.addEventListener(
+//     "load",
+//     () => {
+//       updateMemeCanvas(
+//         topTextInput.value,
+//         bottomTextInput.value,
+//         canvas,
+//         img
+//       );
+//     },
+//     { once: true }
+//   );
+// });
+
+// function updateMemeCanvas( topText, bottomText, canvas, img){
+//   //set canvas to dimentions of image
+//   const context = canvas.getContext('2d');
+//   const height = img.height;
+//   const width = img.width;
+//   //scale text size
+//   const fontSize = Math.floor(width /10);
+//   // put some space above and below the text
+//   const yOffset = height / 25;
+  
+//   // set  font text
+//   context.textAlign = 'center';
+//   context.fontSize =`${fontSize}px impact`;
+//   context.strokestyle = 'black';
+//   context.lineWidth = Math.floor(fontSize / 3);
+//   context.fillStyle = 'white';
+
+//   //update background with canvas size
+//   context.drawImage(img, 0,0);
+//   canvas.height = height;
+//   canvas.width = width;
+
+//   //add top text, pass it into the function
+//   context.textBaseline = 'top';
+//   context.strokeText(topText, width /2, yOffset);
+//   context.fillText(topText, width / 2, height - yOffset);
+
+
+//   // Add bottom text
+//   context.textBaseline = "bottom";
+//   context.strokeText(bottomText, width / 2, height - yOffset);
+//   context.fillText(bottomText, width / 2, height - yOffset);
+// }
+// topTextInput.addEventListener("change", () => {
+//   updateMemeCanvas( topTextInput.value, bottomTextInput.value. canvas, img);
+// });
+
+// bottomTextInput.addEventListener("change", () => {
+//   updateMemeCanvas( topTextInput.value, bottomTextInput.value, canvas, img);
+// });
+
+
+
+const topTextInput = document.querySelector("#topTextInput");
+const bottomTextInput = document.querySelector("#bottomTextInput");
+const imageFileInput = document.querySelector("#imageFileInput");
+const canvas = document.querySelector("#meme");
+let image;
+
+topTextInput.addEventListener("change", () => {
+  updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
+});
+
+bottomTextInput.addEventListener("change", () => {
+  updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
+});
+
+imageFileInput.addEventListener("change", (e) => {
+  const imageDataUrl = URL.createObjectURL(e.target.files[0]);
+
+  image = new Image();
+  image.src = imageDataUrl;
+
+  image.addEventListener("load", () => {
+      updateMemeCanvas(canvas, image, topTextInput.value,bottomTextInput.value);
+    },
+    { once: true });
+});
+
+
+function updateMemeCanvas(canvas, image, topText, bottomText) {
+  const context = canvas.getContext("2d");
+  const width = image.width;
+  const height = image.height;
+  const fontSize = Math.floor(width / 10);
+  const yOffset = height / 25;
+
+  // Update canvas background
+  canvas.width = width;
+  canvas.height = height;
+  context.drawImage(image, 0, 0);
+
+  //  set  font text
+  context.strokeStyle = "black";
+  context.lineWidth = Math.floor(fontSize / 4);
+  context.fillStyle = "white";
+  context.textAlign = "center";
+  context.lineJoin = "round";
+  context.font = `${fontSize}px impact`;
+
+  // add top text, pass it into the function
+  context.textBaseline = "top";
+  context.strokeText(topText, width / 2, yOffset);
+  context.fillText(topText, width / 2, yOffset);
+
+  // Add bottom text
+  context.textBaseline = "bottom";
+  context.strokeText(bottomText, width / 2, height - yOffset);
+  context.fillText(bottomText, width / 2, height - yOffset);
 }
-    reader.readAsDataURL(input.files[0])
-}, false)
