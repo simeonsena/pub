dbce9b27da8c99cf4a26bc3506850fbea5fbe76e
simeonsena <simeon.sena@gmail.com>
2021-11-08 22:31:48 -0500

    forEach, map and filter exercises

--- Module 8/forEach, map, and filter Exercises/foreach-map-filter.js
+++ Module 8/forEach, map, and filter Exercises/foreach-map-filter.js
@@ -133,7 +133,10 @@ Examples:
 */
 
 function extractKey(arr, key){
-    
+          return arr.map(function(name){
+          return name[key]
+      })
+  
 }
 
 /*
@@ -144,7 +147,9 @@ Examples:
 */
 
 function extractFullName(arr){
-    
+    return arr.map(function(name){
+        return name.first + ' ' + name.last;
+    });
 }
 
 /*
@@ -154,7 +159,11 @@ Examples:
     filterByValue([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner') // [{first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Colt', last:"Steele", isCatOwner: true}]
 */
 
-function filterByValue(arr, key) {}
+function filterByValue(arr, key) {
+   return arr.filter(function(value){
+    return value[key] !==undefined;
+   }) ;
+}
 
 /*
 Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
@@ -164,7 +173,13 @@ Examples:
     find([1,2,3,4,5], 10) // undefined
 */
 
-function find(arr, searchValue) {}
+function find(arr, searchValue) {
+    return arr.filter(function(same){
+         return same === searchValue;
+    })
+     [0];
+   
+}
 
 /*
 Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
@@ -173,7 +188,12 @@ Examples:
     findInObj([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true) // {first: 'Tim', last:"Garcia", isCatOwner: true}
 */
 
-function findInObj(arr, key, searchValue) {}
+function findInObj(arr, key, searchValue) {
+    return arr.filter(function(first){
+        return first[key] === searchValue
+    })
+    [0]; 
+}
 
 /*
 Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
@@ -184,7 +204,15 @@ Examples:
     removeVowels('ZZZZZZ') // ('zzzzzz')
 */
 
-function removeVowels(str) {}
+function removeVowels(str) {
+    const vowels= "aeouiAEIOU";
+    let toLower = str.toLowerCase();
+    let splitVowels = toLower.split("");
+    return splitVowels.filter(function(v){
+return vowels.indexOf(v) === -1;
+    })
+    .join("");
+}
 
 /*
 Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and filter to double and then filter the odd numbers).
@@ -194,4 +222,11 @@ Examples:
     doubleOddNumbers([4,4,4,4,4]) // []
 */
 
-function doubleOddNumbers(arr) {}
+function doubleOddNumbers(arr) {
+    return arr.filter(function(v){
+return v % 2 !==0;
+    })
+    .map(function(v){
+        return v * 2;
+    });
+}
