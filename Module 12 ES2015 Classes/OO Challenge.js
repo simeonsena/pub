fcc1ca8457a87aba02859bcd015197e6c0183151
simeonsena <simeon.sena@gmail.com>
2022-01-18 21:22:09 -0500

    oo exercise

--- Module 12 ES2015 Classes/OO Challenge.js
+++ Module 12 ES2015 Classes/OO Challenge.js
@@ -1,16 +1,47 @@
 class Vehicle{    
-    make(){
-        console.log('Hello from triangle!!!@@@')
+    constructor(make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
     }
-    model(){
-        console.log(`Triangle with sides of ${this.a} and ${this.b}`)
-    }
-    year(){
-        console.log('Hello from triangle!!!@@@')
-    }
+   
     honk(){
-        console.log('Beep')
+        return 'Beep';
+    }
+    toString(){
+        return `The vechicl is a ${this.make} ${this.model} from ${this.year}.`;
     }
 }
-let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
-myFirstVehicle.honk(); // "Beep."
+
+class Car extends Vehicle{
+    constructor(make,model,year){
+        super(make, model,year);
+        this.numWheels =4;
+    }
+}
+class Motorcycle extends Vehicle{
+constructor(make,model, year){
+    super(make, model,year);
+    this.numWheels =2;
+}
+revEngine(){
+    return 'Vroom!!'
+}
+}
+
+class Garage{
+    constructor(capacity){
+        this.vehicle = [];
+        this.capacity = capacity;
+    }
+    add(newVehicle){
+        if(!(newVehicle instanceof Vehicle)){
+            return 'Only vehicle are allowed in here!';
+        }
+        if(this.vehicles.length >= this.capacity){
+            return "sorry, we're full."
+        }
+        this.vehicles.push(newVehicle);
+        return "Vehicle added!";
+    }
+}
